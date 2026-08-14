1d60f84aa264395fe8c26e59923a3b80f107c65b
Victor Eronmosele <victoreronmosele@gmail.com>
2026-08-14 13:45:31 +0100

    feat: generate android platform plugin code for the package

---
+++ lib/kinde_flutter_sdk_method_channel.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+import 'kinde_flutter_sdk_platform_interface.dart';
+
+/// An implementation of [KindeFlutterSdkPlatform] that uses method channels.
+class MethodChannelKindeFlutterSdk extends KindeFlutterSdkPlatform {
+  /// The method channel used to interact with the native platform.
+  @visibleForTesting
+  final methodChannel = const MethodChannel('kinde_flutter_sdk');
+
+  @override
+  Future<String?> getPlatformVersion() async {
+    final version = await methodChannel.invokeMethod<String>(
+      'getPlatformVersion',
+    );
+    return version;
+  }
+}
